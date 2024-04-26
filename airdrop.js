d125956a3183f6169e8f929c61fe6434a89ccf18
nekomiao123 <447140443@qq.com>
2024-04-26 15:41:22 +0800

    get airdrop

---
+++ airdrop.js
@@ -0,0 +1,24 @@
+import { Connection, Keypair, LAMPORTS_PER_SOL} from "@solana/web3.js";
+import "dotenv/config";
+import base58 from "bs58";
+
+(async () => {
+  const keypair = Keypair.fromSecretKey(base58.decode(process.env.SECRET_KEY))
+
+  const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+
+  const signature = await connection.requestAirdrop(
+    keypair.publicKey,
+    LAMPORTS_PER_SOL
+  );
+
+  const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
+  await connection.confirmTransaction({
+      blockhash,
+      lastValidBlockHeight,
+      signature
+    });
+})();
+
+
+
