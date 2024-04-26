a8a4a708f96ffa82b7ce757e564dd288f5306f63
nekomiao123 <447140443@qq.com>
2024-04-26 15:32:39 +0800

    send transaction

---
+++ send.js
@@ -0,0 +1,55 @@
+import { Keypair, Connection, LAMPORTS_PER_SOL, PublicKey, Transaction,
+SystemProgram, sendAndConfirmTransaction } from "@solana/web3.js"
+import "dotenv/config"
+import base58 from "bs58"
+
+const userKeypair = Keypair.fromSecretKey(base58.decode(process.env.SECRET_KEY))
+
+// Devnet
+const devConnection = new Connection("https://api.devnet.solana.com")
+// My address
+const add = userKeypair.publicKey.toBase58()
+const userAdd = new PublicKey(add)
+
+// receive address
+const toAdd = new PublicKey('49491M1CaWfQgRmr4Bf4cVkwgr1GQFQ4fmpPcx9Ckc5H')
+
+// balance
+let myBalance = await devConnection.getBalance(userAdd)
+let toAddBalance = await devConnection.getBalance(toAdd)
+
+let mySol = myBalance / LAMPORTS_PER_SOL
+let reSol = toAddBalance / LAMPORTS_PER_SOL
+
+console.log(`My ${userAdd} balance ${mySol} SOL`)
+console.log(`Receive ${toAdd} balance ${reSol} SOL`)
+
+// Transaction
+const trans = new Transaction()
+
+const sendSolInstruction = SystemProgram.transfer({
+    fromPubkey: userAdd,
+    toPubkey: toAdd,
+    lamports: 50000000,
+})
+
+trans.add(sendSolInstruction)
+
+let signature = await sendAndConfirmTransaction(devConnection, trans, [
+    userKeypair,
+])
+
+console.log(
+    `Send Successfully`
+)
+
+// balance
+myBalance = await devConnection.getBalance(userAdd, 'confirmed')
+toAddBalance = await devConnection.getBalance(toAdd, 'confirmed')
+
+mySol = myBalance / LAMPORTS_PER_SOL
+reSol = toAddBalance / LAMPORTS_PER_SOL
+
+console.log(`My ${userAdd} balance ${mySol} SOL`)
+console.log(`Receive${toAddBalance} balance ${reSol} SOL`)
+
